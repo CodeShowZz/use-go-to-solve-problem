9d2c806065e9523cf59ad718861435961b0a2121
霖弟弟 <agilebeanisartist@gmail.com>
2021-01-06 17:28:40 +0800

    Create main.go

---
+++ algorithm/stack/main.go
@@ -0,0 +1,40 @@
+package main
+
+type ResizingArrayStack struct {
+	array  [] string
+	length int
+}
+
+func (stack *ResizingArrayStack) isEmpty() bool {
+	return stack.length == 0
+}
+
+func (stack *ResizingArrayStack) size() int {
+	return stack.length
+}
+
+func (stack *ResizingArrayStack) resize(newSize int) {
+	newArray := make([] string, newSize)
+	for i := 0; i < stack.length; i++ {
+		newArray[i] = stack.array[i]
+	}
+	stack.array = newArray
+}
+
+func (stack *ResizingArrayStack) push(item string) {
+	if stack.length == len(stack.array) {
+		stack.resize(2 * len(stack.array))
+	}
+	stack.array[stack.length] = item
+	stack.length++
+}
+
+func (stack *ResizingArrayStack) pop() string {
+	item := stack.array[stack.length-1]
+	stack.array[stack.length-1] = ""
+	stack.length--
+	if stack.length > 0 && stack.length == len(stack.array)/4 {
+		stack.resize(len(stack.array) / 2)
+	}
+	return item
+}
